cc66e9411680de83fc90ce13dbcea347257d3e41
Chris Cameron-Hann <ccameronhann@gmail.com>
2025-05-29 11:23:07 +0100

    Updates

---
+++ _next/static/chunks/app/(main)/about/page-6ec4e91cd9f76551.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[214],{4662:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4662)}),_N_E=n.O()}]);
--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://camerhann.github.io',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-} 
